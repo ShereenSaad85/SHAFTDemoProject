1a526c77f387671fbe010c1c18806d8634c0a1c9
ShereenSaad85 <shereen.saad1421@gmail.com>
2023-08-15 15:34:13 +0300

    changes

--- src/test/java/verifications/Register.java
+++ src/test/java/verifications/Register.java
@@ -7,73 +7,39 @@ import org.testng.annotations.Test;
 import pages.*;
 
 public class Register {
-//    HomePage homePage;
-//    MainMenuPage mainMenuPage;
-//    SignUpLoginPage signUpLoginPage;
-//    SignUpPage signUpPage;
-//    ContinuePage continuePage;
-//    DeletePage deletePage;
-//SHAFT.GUI.WebDriver driver;
-SHAFT.GUI.WebDriver driver;
-@BeforeMethod
-public void setup(){
-    driver=new SHAFT.GUI.WebDriver();
-//         driver=new ChromeDriver();
-//        driver= DriverFactory.getDriver("Chrome",true);
-//         driver.manage().window().maximize();
-}
+
+    SHAFT.GUI.WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new SHAFT.GUI.WebDriver();
+    }
 
     @Test
     public void RegisterUser() {
-
         new HomePage(driver)
                 .NavigateTo("http://automationexercise.com");
-//                .HomePageIsVisiableAssertion("Automation Exercise");
 
         new MainMenuPage(driver)
                 .LoginSignUpLinkFromHome();
 
         new SignUpLoginPage(driver)
-                .SignUpWithNameAndEmail("Shereen","shereen101@gmail.com");
+                .SignUpWithNameAndEmail("Shereen", "shereen101@gmail.com");
 
         new SignUpPage(driver)
                 .EnterAccountInformationIsVisible("ENTER ACCOUNT INFORMATION")
-                .FillAccountInformation("12345","4","5","2000")
+                .FillAccountInformation("12345", "4", "5", "2000")
                 .CheckBoxes()
-                .FillAddressInformation("Shereen","Saad","VOIS","Dallah","Smart Village","Canada","Giza","hadayek alahram","122456","01116281020");
+                .FillAddressInformation("Shereen", "Saad", "VOIS", "Dallah", "Smart Village", "Canada", "Giza", "hadayek alahram", "122456", "01116281020");
 
         new ContinuePage(driver)
                 .AccountCreatedIsVisible("ACCOUNT CREATED!")
                 .ClickOnContinueButton();
 
-     /*   new MainMenuPage(driver)
-                .LoggedInAsUsernameIsVisible("Logged in as Shereen")
-                .ClickOnDeleteButton();
-
-
-        new DeletePage(driver)
-                .AccountDeletedIsVisible();*/
     }
-    //    ---------------Configurations---------------
-//    @BeforeMethod
-//    public void setup(){
-//        driver=new ChromeDriver();
-////        driver.navigate().to("http://automationexercise.com");
-//        driver.manage().window().maximize();
-////         homePage =new HomePage(driver);
-////         mainMenuPage=new MainMenuPage(driver);
-////         signUpLoginPage=new SignUpLoginPage(driver);
-////         signUpPage =new SignUpPage(driver);
-////         continuePage=new ContinuePage(driver);
-////         deletePage= new DeletePage(driver);
-//
-//    }
-//    @AfterMethod
-//    public void tearDown(){
-//        driver.quit();
-//    }
+
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 }
